eb08066e511cb5b67f498f06ead44e91e8f86faa
Hanish <ochani65@gmail.com>
2020-11-19 18:23:51 +0530

    from hanish

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello{
 	
 	public static void main(String a[]){
 	System.out.print("Hello from saksham");
+	System.out.print("Hello from Hanish");
+
 	
 	
 	}
--- Hello1.java
+++
@@ -1,7 +0,0 @@
-public class Hello1{
-
-public static void main(String args[])
-{
-System.out.println("Hello user is rohan");
-}
-}
