ebf6e15d3e7ffaddc1d6e16199713a933b67a919
sejalnair <sejalnair9@gmail.com>
2020-11-19 18:05:46 +0530

    commitby sejal

--- Hello.java
+++ Hello.java
@@ -1,15 +1,10 @@
 import java.util.*;
 public class Hello{
 	
-	{
-		System.out.print("HEE");
-		
-	}
-
 	public static void main(String a[]){
 	System.out.print("Hello");
 	
-	ArrayList al = new ArrayList();
+	
 	}
 	
 }
