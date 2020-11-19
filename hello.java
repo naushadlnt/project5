d6279bd9860057e291124148b9d02da9e35fd0e0
parshva2198 <parshva.dedhia@somaiya.edu>
2020-11-19 18:26:55 +0530

    p11

--- hello.java
+++ hello.java
@@ -1,15 +1,7 @@
-
 public class Hello{
-	
-	{
-		System.out.print("HEE");
-		
-	}
 
 	public static void main(String a[]){
-	System.out.print("Hello");
-	
-	
+	System.out.print("Hello from Vishal");
 	}
 	
 }
